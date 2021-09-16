0c067953b15dcafa27990e409f347979744702f8
jveiiga <jeferson.veiiga@icloud.com>
2021-09-16 20:03:10 -0300

    Implementing a registration form

--- web/src/components/Promotion/Search/Search.js
+++ web/src/components/Promotion/Search/Search.js
@@ -17,7 +17,7 @@ const PromotionSearch = () => {
 
 
     useEffect(() => {
-      axios.get('http://localhost:5000/promotions?_embed=comments', { params })
+      axios.get('http://localhost:5000/promotions?_embed=comments&_order=desc&_sort=id', { params })
         .then((response) => {
           setPromotions(response.data);
         });
--- web/src/pages/Promotion/Form/Form.js
+++ web/src/pages/Promotion/Form/Form.js
@@ -1,12 +1,16 @@
 import React from 'react'
 import { useParams } from 'react-router';
 
+import PromotionForm from 'components/Promotion/Form/Form';
+import UIContainer from 'components/UI/Container/Container';
+
 const PagesPromotionForm = () => {
     const { id } = useParams();
+    
     return (
-        <div>
-            {id && <div>id: {id}</div>}
-        </div>
+            <UIContainer>
+                <PromotionForm />
+            </UIContainer>
     );
 };
 
